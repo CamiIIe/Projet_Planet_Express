b91368d2aced577839f9155fe4571d1f6201d6fc
Camille ROUMIER <cr.roumier.camille@gmail.com>
2021-03-12 19:18:08 +0100

    InscriptionActivity, ConnexionActivity et background layout

--- app/src/main/java/com/example/projet_planet_express/InscriptionActivity.java
+++ app/src/main/java/com/example/projet_planet_express/InscriptionActivity.java
@@ -2,49 +2,63 @@ package com.example.projet_planet_express;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class InscriptionActivity extends AppCompatActivity {
-/*
+
     EditText nom;
     EditText prenom;
-    EditText mail;
-    EditText date_naissance;
+    EditText identifiant;
+    EditText dateNaissance;
     EditText mdp;
     EditText confirmMDP;
 
-    Button btn_date;
+    DatePickerDialog pickerDialog;
+
     Button valider;
     Button annuler;
-*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
-/*
+
         nom = findViewById(R.id.et_nom);
         prenom = findViewById(R.id.et_prenom);
-        mail = findViewById(R.id.et_mail);
-        date_naissance = findViewById(R.id.et_date_naissance);
+        identifiant = findViewById(R.id.et_identifiant);
+        dateNaissance = findViewById(R.id.et_date_naissance);
         mdp = findViewById(R.id.et_password);
         confirmMDP = findViewById(R.id.et_confirm_password);
 
-        btn_date = findViewById(R.id.btn_date);
         valider = findViewById(R.id.btn_valider);
         annuler = findViewById(R.id.btn_annuler);
-*/
-        /*
-        btn_date.setOnClickListener(new View.OnClickListener() {
+
+        dateNaissance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //selectDate();
-                //TODO
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                pickerDialog = new DatePickerDialog(InscriptionActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dateNaissance.setText(day + " " + month + " " + year);
+
+                    }
+                } , year, month, day);
+                pickerDialog.show();
             }
         });
-    */
+
     }
 
     /*
